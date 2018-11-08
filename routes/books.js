c090fd1b48a2069bc4657c7f4c75b8d3fa77f75d
Kevin Mendez <kevnm24@gmail.com>
2018-11-08 09:48:08 -0800

    added code

---
+++ routes/books.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var sqlite3 = require('sqlite3');
+const Book = require('../models').Book;
+
+// add new book form
+router.get('/books/new', function (req, res, next) {
+  res.render('new-book', { book: Book.build(), title: "New Book" })
+})
+
+// // POST create book
+// router.post('/', function(req, res, next) {
+//   Book.create(req.body).then(function(book) {
+//     res.redirect('/books/' + book.id);
+//   })
+// })

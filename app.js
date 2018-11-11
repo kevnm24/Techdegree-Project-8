1b5ec98927e1299cbf0bdd838828809fbe768e9e
Kevin Mendez <kevnm24@gmail.com>
2018-11-11 12:14:14 -0800

    completed meets

--- app.js
+++ app.js
@@ -18,27 +18,21 @@ app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes)
 app.use('/books', books)
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+// catch 404 and forward to error handler
+app.use(function(req, res) {
+  res.status(400);
+  res.render('page-not-found');
+});
 
-// production error handler
-// no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
-
+// catch 500 error
+app.use(function(error, req, res, next) {
+  res.status(500);
+  res.render('error');
+});
 
 module.exports = app;
--- routes/books.js
+++ routes/books.js
@@ -1,17 +1,91 @@
 var express = require('express');
 var router = express.Router();
-var sqlite3 = require('sqlite3');
-const Book = require('../models').Book;
+const Book = require("../models").Book;
 
-// add new book form
-router.get('/books/new', function (req, res, next) {
-  res.render('new-book')
+// Get books listing
+router.get('/', function(req, res, next) {
+  Book.findAll({order: [['title', 'ASC']]}).then(function(books) {
+    res.render('index', {books: books})
+  }).catch(function(err) {
+    res.send(500)
+  })
 })
 
+// add new book form
+router.get('/new', function (req, res, next) {
+  res.render('new-book', { book: Book.build()})
+})
+
+// POST create book
+router.post('/', function(req, res, next) {
+  Book.create(req.body).then(function(book) {
+    res.redirect('/books');
+  }).catch(function(err) {
+    if(err.name === "SequelizeValidationError") {
+      res.render('new-book', {
+        book: Book.build(req.body),
+        errors: err.errors
+      })
+    } else {
+      throw err
+    }
+  }).catch(function(err) {
+    res.send(500)
+  })
+})
+
+// POST Update book
+router.post('/:id', function(req, res, next) {
+  Book.findById(req.params.id).then(function(book) {
+    if(book) {
+      return book.update(req.body)
+    } else {
+      res.send(404)
+    }
+  }).then(function(book) {
+    res.redirect('/books')
+  }).catch(function(err) {
+    if(err.name === "SequelizeValidationError") {
+      var book = Book.build(req.body)
+      book.id = req.params.id
+      res.render('update-book', {
+        book: book,
+        errors: err.errors
+      })
+    } else {
+      throw err
+    }
+  }).catch(function(err) {
+    res.send(500)
+  })
+})
+
+
+// Delete individual article
+router.post('/:id/delete', function(req, res, next) {
+  Book.findById(req.params.id).then(function(book) {
+    if(book) {
+      return book.destroy()
+    } else {
+      res.send(404)
+    }
+  }).then(function() {
+    res.redirect('/books')
+  }).catch(function(err) {
+    res.send(500)
+  })
+})
+
+// Get individual book
+router.get('/:id', function(req, res, next) {
+  Book.findById(req.params.id).then(function(book) {
+    if(book) {
+      res.render('update-book', {book: book, title: book.title})
+    } else {
+      res.send(404)
+    }
+  }).catch(function(err) {
+    res.send(500)
+  })
+})
 module.exports = router;
-// // POST create book
-// router.post('/', function(req, res, next) {
-//   Book.create(req.body).then(function(book) {
-//     res.redirect('/books/' + book.id);
-//   })
-// })
